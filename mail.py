bf03e617077c93259a94e2e11199758e0f3c4b48
Mukul <vmukul41@gmail.com>
2020-05-26 04:03:32 +0530

    final commit

--- mail.py
+++ mail.py
@@ -1,11 +1,11 @@
 f= open("/root/MLops-task/acc.txt" ,"r")
 a=f.read()
-import getpass
+
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 host_address = "vmukul41@gmail.com"
-host_pass = getpass.getpass("enter your password:")
+host_pass = "mukul@verma3030"
 guest_address = "mukulkumar@krishnacollege.ac.in"
 subject = "Regarding Success of your model "
 content = "congratulations your model trained successfully with"+ str(a)+ "accuracy";
