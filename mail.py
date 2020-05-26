cd627f23089daf5a4179a076f83bf9ec926db870
vmukul41 <54748098+vmukul41@users.noreply.github.com>
2020-05-26 18:20:05 +0530

    last

--- mail.py
+++ mail.py
@@ -4,9 +4,9 @@ a=f.read()
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-host_address = "vmukul41@gmail.com"
-host_pass = "mukul@verma3030"
-guest_address = "mukulkumar@krishnacollege.ac.in"
+host_address = "*****@gmail.com"
+host_pass = "***********"
+guest_address = "*************@krishnacollege.ac.in"
 subject = "Regarding Success of your model "
 content = "congratulations your model trained successfully with"+ str(a)+ "accuracy";
 message = MIMEMultipart()
